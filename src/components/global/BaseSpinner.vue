f894fb3230aaec72b84cb97ebd9b4b128041fc40
g4br-4d3v <gabrielalbuquerque-ads@hotmail.com>
2020-08-06 13:22:40 -0300

    :sparkles: BaseSpinner component

---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/app.scss';
+
+.base-spinner {
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+
+  z-index: 1000000;
+
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: $dark;
+  color: $featured;
+}
+</style>
