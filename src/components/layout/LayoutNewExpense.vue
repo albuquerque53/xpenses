9aeda0d5ddc0a63ea4a7c2a7fb735a653598e480
g4br-4d3v <gabrielalbuquerque-ads@hotmail.com>
2020-08-12 14:22:09 -0300

    :boom: Sending data to Firebase

--- src/components/layout/LayoutNewExpense.vue
+++ src/components/layout/LayoutNewExpense.vue
@@ -5,30 +5,49 @@
       New Expense
     </button>
 
-    <!-- Modal Content -->
-    <div class="modal fade"
-      :style="{ display: modalDisplay }"
-      :class="{ show: showModal }"
-      style="display: block; padding-right: 17px;"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLiveLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="changeModal(false)">
-              <span aria-hidden="true">×</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Woohoo, you're reading this text in a modal!</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="changeModal(false)">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+    <form @submit.prevent="submit()">
+      <!-- Modal Content -->
+      <div class="modal fade"
+        :style="{ display: modalDisplay }"
+        :class="{ show: showModal }"
+        style="display: block; padding-right: 17px;"
+        >
+        <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Add a new expense</h5>
+              <button type="button" class="close" @click="changeModal(false)">
+                <span aria-hidden="true">×</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <div class="form-group col-8">
+                  <label for="">Description</label>
+                  <input class="form-control" type="text" v-model="form.description" required>
+                </div>
+                <div class="form-group col-4">
+                  <label for="">Value</label>
+                  <input class="form-control" type="text" v-model="form.value" required>
+                </div>
+            </div>
+              <p class="text text-muted">Add a brief description accompained by the expense value</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button"
+                class="btn btn-secondary"
+                @click="changeModal(false)"
+                >
+                  Close
+              </button>
+              <button class="btn btn-primary">
+                Add Expense
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
     <!-- Background fade -->
     <div
       class="modal-backdrop fade"
@@ -43,7 +62,12 @@ export default {
   data () {
     return {
       showModal: false,
-      modalDisplay: 'none'
+      modalDisplay: 'none',
+
+      form: {
+        description: '',
+        value: ''
+      }
     }
   },
 
@@ -51,10 +75,41 @@ export default {
     changeModal (value) {
       this.showModal = value
       this.modalDisplay = value ? 'block' : 'none'
+    },
+
+    submit () {
+      this.$root.$emit('Spinner::show')
+
+      const ref = this.$firebase.database().ref(window.uid)
+      const id = ref.push().key
+
+      const payload = {
+        id,
+        receipt: null,
+        value: this.form.value,
+        createdAt: new Date().getTime(),
+        description: this.form.description
+      }
+
+      ref.child(id).set(payload, err => {
+        this.$root.$emit('Spinner::hide')
+
+        if (err) {
+          console.error(err)
+        } else {
+          this.changeModal(false)
+        }
+      })
+
+      this.form.description = ''
+      this.form.value = ''
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.modal {
+  color: var(--darker);
+}
 </style>
