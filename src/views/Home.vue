bb4b9727ea65fba4e42285ce3ce32cff3821c5ac
g4br-4d3v <gabrielalbuquerque-ads@hotmail.com>
2020-08-21 09:54:37 -0300

    :art: Finished Home layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="home">
+    <div class="row">
+      <div class="col-6 home-box">
+        <small>You Spent:</small>
+        <div class="money">R$ 120,00</div>
+        <small>In purchases</small>
+      </div>
+      <div class="col-6 home-box">
+        <small>The average expenditure is:</small>
+        <div class="money">R$ 12,00</div>
+      </div>
+      <div class="col-6 home-box">
+        <small>The largest expense was:</small>
+        <div class="money">R$ 90,00</div>
+        <small>On 12/07/2020</small>
+      </div>
+      <div class="col-6 home-box">
+        <small>The lowest expense was:</small>
+        <div class="money">R$ 4,00</div>
+        <small>On 10/07/2020</small>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,10 +44,37 @@ export default {
       ref.on('value', data => {
         const values = data.val()
 
-        this.expenses = values
-        console.log(this.expenses)
+        console.log(Object.keys(values).map(i => values[i]))
       })
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  .home-box {
+    width: 100%;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 30pt;
+    small {
+      font-size: 1.3rem;
+    }
+    .money {
+      color: var(--featured);
+      margin: 8px;
+    }
+    border: 1px solid var(--dark-medium);
+    &:nth-child(2), &:nth-child(4) {
+      border-left: none;
+    }
+    &:nth-child(3), &:nth-child(4) {
+      border-top: none;
+    }
+  }
+}
+</style>
